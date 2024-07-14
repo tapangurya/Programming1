1552567939995c80a012707a10d5bf50a03c3850
tapangurya <tapangurya1234@gmail.com>
2024-07-14 23:08:57 +0530

    commit12

--- ArrayPractice/Program2.java
+++ ArrayPractice/Program2.java
@@ -1,5 +1,7 @@
 package ArrayPractice;
 
+import java.util.Scanner;
+
 public class Program2 {
     // read size from the user 
     // create int[] for user provided size 
@@ -7,5 +9,22 @@ public class Program2 {
     // print all the data present in the array 
 
     public static void main(String[] args) {
+          Scanner s = new Scanner(System.in) ; 
+        // read size 
+        System.out.println("Enter size : ");
+        int size  = s.nextInt()  ; 
+       // creating array object 
+        int[] a = new int[size] ; 
+        // read elements 
+        a[4] = 20 ; // Compile Time Success 
+        // Exception--->  ArrayIndexOutOfBoundsException
+
+        for( int index = 0 ; index < a.length ; index++ ){
+            a[index] = s.nextInt() ; 
+        } 
+        // print array elements 
+        for( int index = 0 ; index < a.length ; index++){
+            System.out.print(a[index] + ", ");
+        }
     }
 }
