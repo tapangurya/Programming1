d3b4c6591a5811823bdd794a9b16e6eb6a377e03
tapangurya <tapangurya1234@gmail.com>
2024-07-14 22:58:25 +0530

    add4

---
+++ ArrayPractice/Program1.java
@@ -0,0 +1,13 @@
+/*
+ *  To create an Array 
+ * 
+ *  case 1: create array using declare & initialization stmt
+ *  case 2: create an array using new without initialization
+ *  case 3: create an array using new & initialization 
+ *   
+ */
+package ArrayPractice;
+
+public class Program1 {
+    
+}
