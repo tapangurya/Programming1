b53e991da718ab4036af9acfa25ab59b4ff2a893
tapangurya <tapangurya1234@gmail.com>
2024-07-14 23:07:03 +0530

    commit12

---
+++ ArrayPractice/Program2.java
@@ -0,0 +1,11 @@
+package ArrayPractice;
+
+public class Program2 {
+    // read size from the user 
+    // create int[] for user provided size 
+    // initialize data by reading it from the user 
+    // print all the data present in the array 
+
+    public static void main(String[] args) {
+    }
+}
